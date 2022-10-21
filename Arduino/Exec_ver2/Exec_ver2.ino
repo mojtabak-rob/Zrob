1eb741c1dc8b66185247a4f77de1fcc006135384
Joseph Clemente <jpclemente97@Josephs-MacBook-Pro-462.local>
2022-10-21 23:42:42 +0200

    Cleaned up exec v2 code

--- Arduino/Exec_ver2/Exec_ver2.ino
+++ Arduino/Exec_ver2/Exec_ver2.ino
@@ -48,48 +48,31 @@ enum HitType {
   both
 };
 
+HitType rhythm1[2] = {green,black};
+HitType rhythm2[2] = {green,black};
+HitType rhythm3[2] = {green,green};
+
 // Moves a bot to a specified position in the defined position
-
-void move (long bot, long dir, long pos) {
+void move (long bot, long dir, long pos, bool slow = false) {
   buf[0]= 0xA6;
   buf[1]= dir;
-  buf[2]= 0x20;
-  buf[3]= 0x20;
+  buf[2]= slow ? 0x01: 0x20;
+  buf[3]= slow ? 0x01: 0x20;
   buf[4]= pos;
   buf[5]= pos >> 8;
   buf[6]= 0x00;
   buf[7]= 0x00;
   CAN.sendMsgBuf(bot,0,8,buf);
 
-  while (true){
-        if (CAN_MSGAVAIL == CAN.checkReceive()){
-          CAN.readMsgBuf(&len,buf);
-          break;  
-        }
-      }
-}
-
-void moveslow (long bot, long dir, long pos) {
-  buf[0]= 0xA6;
-  buf[1]= dir;
-  buf[2]= 0x01;
-  buf[3]= 0x01;
-  buf[4]= pos;
-  buf[5]= pos >> 8;
-  buf[6]= 0x00;
-  buf[7]= 0x00;
-  CAN.sendMsgBuf(bot,0,8,buf);
-
-  while (true){
-        if (CAN_MSGAVAIL == CAN.checkReceive()){
-          CAN.readMsgBuf(&len,buf);
-          break;  
-        }
-      }
+  while (true) {
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {
+      CAN.readMsgBuf(&len,buf);
+      break;  
+    }
+  }
 }
 
 // Reads the current position of the bot
-
 void readPos (long bot, long initPos) {
   buf[0]= 0x94;
   buf[1]= 0x00;
@@ -113,7 +96,7 @@ void readPos (long bot, long initPos) {
         // Determine position of bot, then move to starting position of 16000
         unsigned int currentPos = buf[6] + (buf[7] << 8);
         long dir = (currentPos > initPos) ? down : up;
-        moveslow(bot, dir, initPos);
+        move(bot, dir, initPos, true);
         break;
       }
     }
@@ -150,8 +133,6 @@ int hit (float greenAmp[], float greenFreq[], float blackAmp[], float blackFreq[
       }
       
       move(greenBot, dirGreen, greenPos);
-
-      
     }
 
     //delay(delayTime);
@@ -195,14 +176,14 @@ void playRhythm(HitType rhythm[], float greenAmp[], float greenFreq[], float bla
   if (sizeof(greenFreq) != 0) {
     int initialPos = greenContactPoint + greenBias + (greenAmp[0] * 2);
     long dir = (initialPos > greenPos) ? up : down;
-    moveslow(greenBot, dir, initialPos);
+    move(greenBot, dir, initialPos, true);
     greenPos = initialPos;
     delay(500);
   }
   if (sizeof(blackFreq) != 0) {
     int initialPos = blackContactPoint + blackBias + (blackAmp[0] * 2);
     long dir = (initialPos > blackPos) ? up : down;
-    moveslow(blackBot, dir, initialPos);
+    move(blackBot, dir, initialPos, true);
     blackPos = initialPos;
     delay(500);
   }
@@ -210,8 +191,6 @@ void playRhythm(HitType rhythm[], float greenAmp[], float greenFreq[], float bla
   int out = 0;
   for (int i = 0; i < 8; ++i) {
     int num = 0;
-    
-
     num = i % 2;
 
     if (digitalRead(DOWN) == LOW) {
@@ -219,8 +198,6 @@ void playRhythm(HitType rhythm[], float greenAmp[], float greenFreq[], float bla
     }
 
     out = hit(greenAmp, greenFreq, blackAmp, blackFreq, rhythm[num], harmonics);
-
-    //wait fr the next move(another input for the function) 
   }
 }
 
@@ -254,13 +231,6 @@ void setup() {
 
   // Read the position of both robots, then decide on a direction after
   // the position is received (>= 16000: up, < 16000: down)
-  
-}
-
-void loop() {
-  HitType rhythm1[2] = {green,black};
-  HitType rhythm2[2] = {green,black};
-  HitType rhythm3[2] = {green,green};
 
   currentBot = greenBot;
   readPos(greenBot,greenPos);
@@ -268,110 +238,21 @@ void loop() {
   currentBot = blackBot;
   readPos(blackBot,blackPos);
   delay(1000);
-
-  while(true) {
-
-    int validFreq=0;
-    int validAmp=0;
-
-    while (!SERIAL.available());
-    //Serial.println("found freq");
-    //int freq = SERIAL.readString().toInt();
-    unsigned long freq = Serial.read();
-    while (!SERIAL.available());
-    //Serial.println("found amp");
-    unsigned long AMP1 = Serial.read();
-    //int AMP1 = SERIAL.readString().toInt();
-    //readPos(greenBot,13000);
-    //delay(1000);
-    float freq1 = float(freq)/1000;
-    AMP1 = AMP1*20;
-    
-    if (freq1>0.01&&freq1<0.04){
-      
-      //readPos(greenBot,10000);
-      //delay(1000);
-      validFreq=1;
-    }
-
-    if (AMP1>0&&AMP1<5000){
-      //readPos(greenBot,13000);
-      //delay(1000);
-        validAmp=1;
-    }
-
-    if (validFreq==1&&validAmp==1){
-      // AMP=[0,5000]
-      //SERIAL.print(1);
-      //while (!SERIAL.available());
-      //int AMP1 = SERIAL.readString().toInt();
-      //if (AMP1>0&&AMP1<5000){
-        //validAmp=1;
-      //}
-      
-      float greenAmp[] = {AMP1};
-      float greenFreq[] = {freq1};
-      float blackAmp[] = {AMP1};
-      float blackFreq[] = {freq1};
-      playRhythm(rhythm1, greenAmp, greenFreq, blackAmp, blackFreq, 1);
-      //SERIAL.print(freq1);
-      //}
-      //else{
-      //  SERIAL.print("fail");
-      //}
-    }
-    //else{
-    //  SERIAL.print("fail");
-    //}
-
-    
-
-
-
-    
-//    if (digitalRead(UP)==LOW) {
-//      // AMP=[0,5000]
-//      float greenAmp[] = {2500};
-//      float greenFreq[] = {0.03};
-//      float blackAmp[] = {2500};
-//      float blackFreq[] = {0.03};
-//      playRhythm(rhythm1, greenAmp, greenFreq, blackAmp, blackFreq, 1);
-//    }
-//    else if (digitalRead(LEFT)==LOW) {
-//      exit(0);
-//      //playRhythm(rhythm2, {3000}, {0.005}, {-3000}, {0.005}, 1);
-//    }
-//    else if (digitalRead(RIGHT)==LOW) {
-//      exit(0);
-//      //playRhythm(rhythm3, {3000}, {0.005}, {-3000}, {0.005}, 1);
-//    }
-  }
 }
 
-//SERIAL read:
-//  while (!SERIAL.available());
-//      freq = SERIAL.readString().toInt();
+void loop() {
+  while (!SERIAL.available());
+  unsigned long freq = Serial.read();
+  while (!SERIAL.available());
+  unsigned long AMP1 = Serial.read();
+  float freq1 = float(freq)/1000;
+  AMP1 = AMP1*20;
 
-
-
-
-
-
-//  if (CAN_MSGAVAIL == CAN.checkReceive())
-//  {
-//    CAN.readMsgBuf(&len,buf);
-//
-//    unsigned long canID = CAN.getCanId();
-//
-//    for (int i = 0; i<len; i++)
-//    {
-//      SERIAL.print(buf[i],HEX);
-//      SERIAL.print("\t");
-//    }
-//    SERIAL.print(greenPos);
-//    SERIAL.print("\t");
-//    SERIAL.print(blackPos);
-//    SERIAL.print("\t");
-//    SERIAL.print(canID,HEX);
-//    SERIAL.println();
-//  }
+  if ((freq1 > 0.01) && (freq1 < 0.04) && (AMP1 > 0) && (AMP1 < 5000)) {      
+    float greenAmp[] = {AMP1};
+    float greenFreq[] = {freq1};
+    float blackAmp[] = {AMP1};
+    float blackFreq[] = {freq1};
+    playRhythm(rhythm1, greenAmp, greenFreq, blackAmp, blackFreq, 1);
+  }
+}
